a9fa4d3472212536d5bba2dd40cb985eb956a8f5
thallyson1pontes <thallyson.salazar@hotmail.com>
2025-12-01 15:02:47 -0300

    SCAD-0001[Inclui config de seguraça tipo basic security, inclui no pom configuração de segurança do spring config]

---
+++ src/main/java/br/com/scad/scad/config/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package br.com.scad.scad.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    // todo criaçao de um bean do tipo SecurityFilterChain que injeta o HttpSecurity  e cria um form padrão e um basic
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable) //todo  desabilita a config para fazer a seguraça do front onde o front é obrigado a enviar um token para o back
+                .formLogin(configurer -> configurer.loginPage("login.html").successForwardUrl("home.html")) //todo habilida o form padrão e injeta um target para a pagina
+                .formLogin(Customizer.withDefaults()) //todo adiciona o form padrão
+                .httpBasic(Customizer.withDefaults()) //todo adiciona o autenticação basic
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()) // todo qualquer requisição deve estar autenticado
+                .build();
+
+    }
+}
