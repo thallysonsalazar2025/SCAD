3e08acb8bc7d017cd9abb39df95f20c5d3f57347
thallyson1pontes <thallyson.salazar@hotmail.com>
2025-12-02 11:36:33 -0300

    SCAD-0001[Inclui autenticação criação de repositorio em memoria e criptografia de password]

--- src/main/java/br/com/scad/scad/config/SecurityConfiguration.java
+++ src/main/java/br/com/scad/scad/config/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,5 +33,30 @@ public class SecurityConfiguration {
                         authorizeRequests.anyRequest().authenticated()) // todo qualquer requisição deve estar autenticado
                 .build();
 
+    }
+    //todo cria um Bean que faz a critografia da senha atraves da,
+    // interface PasswordEncoder e retorna um obj do tipo BCryptPasswordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+    //todo Cria um bean para criar usuarios em mémoria e injeta o PasswordEncoder para capturar o password e criptografar a senha
+    // repositorio de usuarios em memoria!***
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        UserDetails user1 = User.builder()
+                .username("usuario")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("usuario2")
+                .password(encoder.encode("456"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
     }
 }
