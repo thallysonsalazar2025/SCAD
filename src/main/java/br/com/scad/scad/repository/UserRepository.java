b197b8d2bbe9d34d318d57a56d85ca4e3be7fe2d
thallyson1pontes <thallyson.salazar@hotmail.com>
2025-12-04 12:15:48 -0300

    SCAD-0001[Inclui melhorias na entidade de response da consulta cadastro e atualização]

--- src/main/java/br/com/scad/scad/repository/UserRepository.java
+++ src/main/java/br/com/scad/scad/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package br.com.scad.scad.repository;
 
-
-
-import br.com.scad.scad.domain.User;
+import br.com.scad.scad.domain.UserDomain;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserDomain, Long> {
+    UserDomain findByLogin(@NotNull String login);
 }
--- src/main/java/br/com/scad/scad/service/mapper/UserMapper.java
+++ src/main/java/br/com/scad/scad/service/mapper/UserMapper.java
@@ -1,11 +1,15 @@
 package br.com.scad.scad.service.mapper;
+import br.com.scad.scad.domain.UserDomain;
 import br.com.scad.scad.generated.model.UserRegistrationRequest;
 
-import br.com.scad.scad.domain.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Mapper(componentModel = "spring", uses = User.class)
+@Mapper(componentModel = "spring")
 public interface UserMapper {
-    UserRegistrationRequest toUserRegistrationRequest(User user);
-    User toUser(UserRegistrationRequest userRegistrationRequest);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", expression = "java(passwordEncoder.encode(user.getPassword()))")
+    UserDomain toUser(UserRegistrationRequest user, PasswordEncoder passwordEncoder);
 }
