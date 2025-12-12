bc4555313f8db34857254c00ef358e31eba88005
thallyson1pontes <thallyson.salazar@hotmail.com>
2025-12-12 11:59:03 -0300

    SCAD-0001[Inclui configuração do authorization server porteiro da aplicação]

--- src/main/java/br/com/scad/scad/config/AuthorizationServerConfiguration.java
+++ src/main/java/br/com/scad/scad/config/AuthorizationServerConfiguration.java
@@ -1,17 +1,48 @@
 package br.com.scad.scad.config;
 
+import org.junit.jupiter.api.Order;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.Duration;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfiguration {
+
+    @Bean
+    @Order(1) //todo qualifica esse bean para ser priorizado na hora de criar e gerenciar pelo spring Security
+    public SecurityFilterChain serverSecurityFilterChain(HttpSecurity http) throws Exception {
+        //todo cria a configuração de Oauth2 do ResourceServer
+        http.authorizeHttpRequests(
+                authorizeRequests -> authorizeRequests.anyRequest().authenticated()
+        ) //todo todas as requisições precisam estar autenticadas e essa linha faz isso
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(withDefaults()))
+                .formLogin(configurer -> configurer.loginPage("/login"))
+                .oauth2Login(withDefaults()
+                        ); // todo adiciona o suporte ao login via oauth2 via open id connect (oidc)
+
+        return http.build();
+    }
+    //todo cria um Bean que faz a critografia da senha atraves da,
+    // interface PasswordEncoder e retorna um obj do tipo BCryptPasswordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
     @Bean
     public TokenSettings tokenSettings() {
         return TokenSettings.builder()
--- src/main/java/br/com/scad/scad/config/SecurityConfiguration.java
+++ src/main/java/br/com/scad/scad/config/SecurityConfiguration.java
@@ -1,8 +1,6 @@
 package br.com.scad.scad.config;
 
-import br.com.scad.scad.security.CustomUserDetailsService;
 import br.com.scad.scad.security.LoginSocialSuccessHandler;
-import br.com.scad.scad.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,14 +10,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true) //todo responsavél por habilitar o controle de regras nos controllers
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+//todo responsavél por habilitar o controle de regras nos controllers
 public class SecurityConfiguration {
 
     // todo criaçao de um bean do tipo SecurityFilterChain que injeta o HttpSecurity  e cria um form padrão e um basic
@@ -28,8 +26,6 @@ public class SecurityConfiguration {
         return http
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable) //todo  desabilita a config para fazer a seguraça do front onde o front é obrigado a enviar um token para o back
-//                .formLogin(configurer -> configurer.loginPage("login.html").successForwardUrl("home.html")) //todo habilida o form padrão e injeta um target para a pagina
-//                .formLogin(Customizer.withDefaults()) //todo adiciona o form padrão
                 .formLogin(configurer -> {
                     configurer.loginPage("/login").permitAll();
                 })
@@ -39,56 +35,28 @@ public class SecurityConfiguration {
                     authorizeRequests.requestMatchers(HttpMethod.POST, "/users/**").permitAll();
                     authorizeRequests.anyRequest().authenticated();
 
-//                    authorizeRequests.requestMatchers(HttpMethod.POST, "/autores/**").hasRole("ADMIN");
-//                    authorizeRequests.requestMatchers(HttpMethod.PUT, "/autores/**").hasRole("ADMIN");
-//                    authorizeRequests.requestMatchers(HttpMethod.POST,"/users/**").hasRole("ADMIN");
-//                    authorizeRequests.requestMatchers(HttpMethod.DELETE, "/autores/**").hasRole("ADMIN");
-//                    authorizeRequests.requestMatchers(HttpMethod.POST, "/books/**").hasRole("ADMIN");
-//                    authorizeRequests.requestMatchers(HttpMethod.PUT, "/books/**").hasRole("ADMIN");
-//                    authorizeRequests.requestMatchers(HttpMethod.DELETE, "/books/**").hasRole("ADMIN");
-                    //todo setando autorização para o usuario ter uma permissão especifica utilizando o método hasAuthority
-//                    authorizeRequests.requestMatchers(HttpMethod.POST, "/books/**").hasAuthority("CADASTRO_USUARIO");
-                    //todo adiciona o autenticação basic sem ela autenticação nao funciona
                 })
+                .oauth2ResourceServer(oauth2rs -> oauth2rs.jwt(Customizer.withDefaults()))
                 .oauth2Login(ouath2 -> ouath2
                         .loginPage("/login")
                         .successHandler(successHandler))
                 .build();
     }
 
-    //todo cria um Bean que faz a critografia da senha atraves da,
-    // interface PasswordEncoder e retorna um obj do tipo BCryptPasswordEncoder
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
-    }
 
-    //todo retorna a configuração da CustomUserDetailsService onde eu crio a configuração de acesso utilizando
-//    @Bean
-    public UserDetailsService userDetailsService(UserService userService) {
-        //todo Cria um bean para criar usuarios em mémoria e injeta o PasswordEncoder para capturar o password e criptografar a senha
-        // repositorio de usuarios em memoria!***
-
-//        UserDetails user1 = User.builder()
-//                .username("usuario")
-//                .password(encoder.encode("123"))
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails user2 = User.builder()
-//                .username("admin")
-//                .password(encoder.encode("456"))
-//                .roles("ADMIN")
-//                .build();
-//        return new InMemoryUserDetailsManager(user1, user2);
-
-        return new CustomUserDetailsService(userService);
-    }
     //todo cria um bean para setar o prexisso da authorenty sem nenhum valor anteriormente ele usava o valor ROLE_
     @Bean
-    public GrantedAuthorityDefaults grantedAuthorityDefaults(){
-        return new  GrantedAuthorityDefaults("");
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("");
     }
-
+    //todo Configura o token JWT e seta o prefixo SCOPE sem nenhum valor
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthorityPrefix(""); // todo set para definir o prefixo que vem do token jtw
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+        return  jwtConverter;
+    }
 
 }
