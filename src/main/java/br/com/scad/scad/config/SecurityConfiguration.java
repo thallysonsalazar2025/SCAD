0d4bf185f937bb37a900678927defdb94dac39d6
thallyson1pontes <thallyson.salazar@hotmail.com>
2025-12-26 15:28:00 -0300

    SCAD-0001[Inclui config do actuator para obter métricas da aplicação]

--- src/main/java/br/com/scad/scad/config/SecurityConfiguration.java
+++ src/main/java/br/com/scad/scad/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,6 +26,18 @@ public class SecurityConfiguration {
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web ->  web.ignoring().requestMatchers(
+                "/api/v1/v2/api-docs/**",
+                "/swagger-resources/**",
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+                "/swagger-ui.html",
+                "/webjars/**",
+                "/actuator/**"
+        );
     }
 
     @Bean
